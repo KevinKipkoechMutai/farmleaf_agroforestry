0d28248bae205c3db96af37d1749c7ef781685e6
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-05-21 19:02:29 +0300

    UI update

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -4,7 +4,7 @@ import { tilled_land } from '../assets'
 
 const Goals = () => {
   return (
-    <div className='flex sm:flex-row flex-col-reverse w-[90%]  m-auto mt-4 '>
+    <div className='flex sm:flex-row flex-col-reverse w-[80%]  m-auto mt-4 '>
       <img src={tilled_land} alt='tilled land' className='w-[40rem] h-[27rem] m-auto rounded'/>
       <div className='flex flex-col items-center justify-between w-[80%] m-auto sm:mt-0 mt-3'>
         <h1 className="font-poppins font-semibold text-gradient text-[50px] sm:text-[40px] mb-3">Why Agroforestry?</h1>
