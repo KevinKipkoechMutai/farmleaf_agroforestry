b79de377fdccf6ff2a049a892aa8696dc79d913c
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-05-21 19:14:13 +0300

    UI update

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -2,7 +2,7 @@
 
 const GoalItem = ({image, statement}) => {
   return (
-    <div className="flex flex-row sm:w-[60%] w-[80%] items-start justify-center m-auto ">
+    <div className="flex flex-row sm:w-[60%] w-[80%] items-start justify-center m-auto mr-0 ">
         <img src={image} alt="goal image" className="h-[4rem] w-[4rem] mr-2 rounded-full"/>
         <div className="flex items-center justify-center m-auto">
             <p>{statement}</p>
--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -4,11 +4,11 @@ import { tilled_land } from '../assets'
 
 const Goals = () => {
   return (
-    <div className='flex sm:flex-row flex-col-reverse w-[80%]  m-auto mt-4 '>
-      <img src={tilled_land} alt='tilled land' className='w-[40rem] h-[27rem] m-auto rounded'/>
-      <div className='flex flex-col items-center justify-between w-[80%] m-auto sm:mt-0 mt-3'>
+    <div className='flex sm:flex-row flex-col-reverse w-[90%]  m-auto mt-4 items-center justify-between'>
+      <img src={tilled_land} alt='tilled land' className='w-[50rem] h-[27rem] m-auto rounded sm:ml-4 ml-0'/>
+      <div className='flex flex-col items-center justify-between sm:w-[70%] w-[100%] m-auto sm:mt-0 mt-3 sm:mb-0 mb-2'>
         <h1 className="font-poppins font-semibold text-gradient text-[50px] sm:text-[40px] mb-3">Why Agroforestry?</h1>
-        <div className='flex flex-col items-center justify-center gap-2'>
+        <div className='flex flex-col items-end justify-around gap-2 mr-5'>
           {advantages.map((item) => (
             <GoalItem key={item.id} {...item} />
           ))}
